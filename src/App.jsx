0ae6acf12332682b7f29a2d8704d33f62b99c9d3
mikail <mikailadev001@gmail.com>
2026-06-15 09:29:09 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  
+
+  return (
+    <div className="main-section">
+      <div className="heading-section">
+        <input type="text" placeholder="Write your name..." />
+        <button>Submit</button>
+      </div>
+
+      <div className="output">
+        <ul>
+          <li>mikaila</li>
+          <li>sani</li>
+          <li>umar</li>
+
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
